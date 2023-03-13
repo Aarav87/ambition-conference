32863e25d0c7f5de35f751bf725cd4e1e1873e37
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-03-13 13:47:14 -0400

    updated navbar + created new pages

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/logo.png"
-import igIcon from "@/public/ig.png"
+import {useRouter} from "next/router";
 
 const menuItems = [
     {
@@ -23,19 +22,39 @@ const menuItems = [
     }
 ]
 
+const socials = [
+    {
+        name: "Instagram",
+        url: "https://www.instagram.com/ambitionconferences/",
+        img: "/socials/ig.png"
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/company/ambition-conferences/",
+        img: "/socials/linkedin.png"
+    },
+    {
+        name: "Email",
+        url: "",
+        img: "/socials/email.png"
+    }
+]
+
 const Navbar = () => {
+    const router = useRouter();
+
     return (
         <header className="bg-white py-2 sticky text-xl">
             <div className="w-full h-20 px-4 mx-auto lg:flex lg:items-center shadow-xl z-[100]">
                 <div className="flex justify-between items-center">
                     <Link href="/" className="p-4">
-                        <Image src={Logo} alt="logo" width={100} height={120} />
+                        <Image src="/logo.png" alt="logo" width={100} height={120} />
                     </Link>
                 </div>
                 <div className="lg:flex flex-col lg:flex-row lg:ml-auto mt-3 lg:mt-0">
                     {menuItems.map((menuItem, i) => (
                         <Link
-                            className="p-2 lg:px-6 lg:mx-2 rounded duration-150 transition-colors hover:text-amber-300"
+                            className={`${menuItem.path === router.route ? "text-amber-300" : "text-black hover:text-amber-300"} p-2 lg:px-4 lg:mx-2 rounded duration-150 transition-colors`}
                             href={menuItem.path}
                             key={i}
                         >
@@ -43,10 +62,12 @@ const Navbar = () => {
                         </Link>
                     ))}
                 </div>
-                <div className="lg:flex flex-row lg:ml-auto mt-3 lg:mt-0">
-                    <a href="https://www.instagram.com/ambitionconferences/">
-                        <Image src={igIcon} alt="Instagram" width={40} height={40} />
-                    </a>
+                <div className="lg:flex flex-row lg:ml-auto mt-3 lg:mt-0 items-center justify-center self-center justify-self-center gap-4">
+                    {socials.map((social, i) => (
+                        <a href={social.url} key={i}>
+                            <Image src={social.img} alt={social.name} width={40} height={40} />
+                        </a>
+                    ))}
                 </div>
             </div>
         </header>
