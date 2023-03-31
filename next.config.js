d5fe181d788cec1961f9cc620118b6f09bad57f8
Aarav <43528744+Aarav87@users.noreply.github.com>
2023-03-30 22:43:43 -0400

    updated next settings

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   },
   transpilePackages: ["next-image-export-optimizer"],
   env: {
-    nextImageExportOptimizer_imageFolderPath: "public/images",
+    nextImageExportOptimizer_imageFolderPath: "public/",
     nextImageExportOptimizer_exportFolderPath: "out",
     nextImageExportOptimizer_quality: 75,
     nextImageExportOptimizer_storePicturesInWEBP: true,
